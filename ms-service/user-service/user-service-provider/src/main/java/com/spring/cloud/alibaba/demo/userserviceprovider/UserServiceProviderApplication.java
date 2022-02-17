a0ea1795c287f39d544fd8b4b86354ac8698a592
lykismekk <lykismekk@outlook.com>
2022-02-17 18:11:12 +0800

    dubbo-rpc-service-provider

---
+++ ms-service/user-service/user-service-provider/src/main/java/com/spring/cloud/alibaba/demo/userserviceprovider/UserServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.alibaba.demo.userserviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceProviderApplication.class, args);
+    }
+
+}
---
+++ ms-service/user-service/user-service-provider/src/test/java/com/spring/cloud/alibaba/demo/userserviceprovider/UserServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.alibaba.demo.userserviceprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
