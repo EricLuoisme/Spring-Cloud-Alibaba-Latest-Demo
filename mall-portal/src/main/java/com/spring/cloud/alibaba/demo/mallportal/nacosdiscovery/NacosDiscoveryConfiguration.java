44372faca49f52139bdc819f6e38309cb0f67944
lykismekk <lykismekk@outlook.com>
2022-02-17 18:06:15 +0800

    Initial commit

---
+++ mall-portal/src/main/java/com/spring/cloud/alibaba/demo/mallportal/nacosdiscovery/NacosDiscoveryConfiguration.java
@@ -0,0 +1,9 @@
+package com.spring.cloud.alibaba.demo.mallportal.nacosdiscovery;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@EnableDiscoveryClient
+@Configuration
+public class NacosDiscoveryConfiguration {
+}
